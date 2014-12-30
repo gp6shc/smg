66ba8a93e6b9cff51a7c3b893723dbbe49cae293
Sachs Live <mike@sachsmedia.com>
2014-12-30 12:26:23 -0500

    server: update uploads

---
+++ wp-content/themes/ronsachs/templates/page-empty.php
@@ -0,0 +1,19 @@
+<?php /* Template Name: Empty */ 
+
+pxl::page(); while ( have_posts() ) : the_post(); ?>
+		<style>
+			.wrapper {
+				width: 100% ;
+			}
+			
+			#primary, #banner, body > div:nth-child(3) {
+				display: none;
+			}
+			body {
+				border: none;
+			}
+		</style>
+		<div><?php the_content(); ?></div>
+		<!--end content-->
+		
+<?php endwhile; pxl::page(0); ?>
