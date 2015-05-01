4380dd3765db8541830e038f24f317373a006572
Mike <mike@sachsmedia.com>
2015-05-01 16:50:06 -0400

    restructured home page, new facebook page plugin

--- wp-content/themes/ronsachs/templates/default/home.php
+++ wp-content/themes/ronsachs/templates/default/home.php
@@ -1,15 +1,16 @@
-<div class="row">
-	<div class="constrain">
-		<p class="intro">
-			Sachs Media Group is Florida's dominant public affairs communications company for critical issues and one of America’s leading independent communications companies. With exceptional experience and results in public affairs, branding, social, digital, and crisis communications, the firm combines unparalleled relationships, news judgment, messaging, and storytelling ability with cutting-edge strategies to engage audiences with content they seek and share.
-		</p>
-	</div>
+<div class="row spaced intro">
+	<p class="span6">
+		<iframe width="449" height="198.4" src="https://www.youtube.com/embed/DWlBwezbkj8?controls=0&modestbranding=1&rel=0&showinfo=0" frameborder="0" allowfullscreen></iframe>
+	</p>
+	<p class="span6">
+		<span class="break-through">Break through.</span> In today’s mind-numbing, message-a-minute world, getting seen and being heard is a big challenge. To overcome the clutter, you need a compelling message, a clear target, and a proven partner to help you break through.
+	</p>
 </div>
 
 <div class="row">
-	<h4>Creative Spotlight</h4>
+	<h4>Our Work</h4>
 	<div id="new-work-slider">
-		<?php echo do_shortcode("[posts type=work show=12 loop=post-slider]"); ?>
+		<?php echo do_shortcode("[posts type=work show=9 loop=post-slider]"); ?>
 	</div>
 </div>
 
@@ -20,23 +21,29 @@
 	</div>
 </div>
 
-<div class="row spaced">
+<div class="row">
 	<div id="hot" class="span6">
 		<h4>Hot Off the Blog</h4>
-		<?php echo do_shortcode("[posts category_name=hotofftheblog show=1 loop=post-blog]"); ?>
+		<?php echo do_shortcode("[posts category_name=hotofftheblog show=2 loop=post-blog]"); ?>
 	</div>
 	<div id="be-social" class="span6">
 		<h4>Let's Be Social</h4>
 		<div class="row lined">
-			<div class="span3">
-				<div class="facebook-box" style="margin-top:-6px;">
+			<div class="span6">
+				<div class="facebook-box">
 					<h3>Facebook Us</h3>
-					<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FSachsMedia&amp;width=222&amp;height=427&amp;show_faces=true&amp;colorscheme=light&amp;stream=false&amp;show_border=false&amp;header=true&amp;appId=175195112639351" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100%; height:420px;" allowTransparency="true"></iframe>
+					<div class="fb-page" data-href="https://www.facebook.com/sachsmedia" data-hide-cover="false" data-show-facepile="true" data-show-posts="false" width="450">
+						<div class="fb-xfbml-parse-ignore">
+							<blockquote cite="https://www.facebook.com/sachsmedia">
+								<a href="https://www.facebook.com/facebook">Facebook</a>
+							</blockquote>
+						</div>
+					</div>
 				</div>
 			</div>
-			<div id="twitter" class="span3" style="margin-top:-2px;">
-				<h3>All ATwitter</h3>
-				<a class="twitter-timeline" width="100%" height="420" href="https://twitter.com/twitterapi" data-widget-id="277096538856099840">@SachsMediaGrp</a>
+			<div id="twitter" class="span6">
+				<h3>All ATwitter <a href="https://twitter.com/SachsMediaGrp" target="_blank" class="twitter-handle">@SachsMediaGrp</a></h3>
+				<a class="twitter-timeline" href="https://twitter.com/SachsMediaGrp" data-chrome="noheader nofooter noborder" data-widget-id="593808855299133442">Tweets by @SachsMediaGrp</a>
 				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 			</div>
 		</div>
