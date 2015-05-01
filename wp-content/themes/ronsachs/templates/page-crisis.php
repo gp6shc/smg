f2221318f8b3a6425455972544d754f519091921
Mike <mike@sachsmedia.com>
2015-05-01 16:46:13 -0400

    new what we do pages, renamed some old

--- wp-content/themes/ronsachs/templates/page-crisis.php
+++ wp-content/themes/ronsachs/templates/page-crisis.php
@@ -1,8 +1,8 @@
-<?php /* Template Name: What We Do: Social/Digital */ pxl::page(); while ( have_posts() ) : the_post(); ?>
+<?php /* Template Name: What We Do: Crisis */ pxl::page(); while ( have_posts() ) : the_post(); ?>
 
 <div class="row lined">
 	<div id="sidebar" class="span3">
-		<?php wp_nav_menu('menu=what-we-do&container=&menu_id=side&menu_class=no-margin'); ?>
+		<?php wp_nav_menu('menu=what-we-do&container=&menu_id=side&menu_class=no-margin'); ?>	
 		<br />
 		<?php if ( !dynamic_sidebar('Sidebar') ) : ?>
 		<?php endif; ?>
@@ -18,7 +18,7 @@
 				<div class="span6">
 					<!--blog section-->
 					<h3 class="related">Related Blog Articles<!--echos first name only--></h3>
-					<p style="padding-bottom:4px;"><?php echo do_shortcode("[posts show=3 loop=post-indivcasestudy category_name=socialdigital]"); ?></p>
+					<p style="padding-bottom:4px;"><?php echo do_shortcode("[posts show=3 loop=post-indivcasestudy category_name=crisiscommunication]"); ?></p>
 					<!--end blog section-->
 				</div><!--end span6-->
 							
@@ -27,11 +27,8 @@
 
 		<!--Case Studies -->
 		<div id="sidebar" class="span3">
-		<!--<h3 class="whatwedo">Featured Resources</h3>
-		<p><a href="http://pages.optify.net/2013-social-media-strategy-guide" target="_blank"><img src="http://sachsmedia.com/wp-content/uploads/2013/02/download.jpg" title="Click to Download" style="margin-top:5px;width:200px;height:115px;"/></a></p>-->
-
 		<h3 class="whatwedo">Case Studies</h3>
-			<?php echo do_shortcode("[posts loop=post-casestudy category_name=casestudy-socialdigital]"); ?></p>
+			<?php echo do_shortcode("[posts loop=post-casestudy category_name=casestudy-crisiscomm]"); ?></p>
  		</div><!--end span 3-->
  		
 </div><!--end container row lined-->
