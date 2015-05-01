cd772aee7c0a2594992aa081a211c47b71a5f8e9
Mike <mike@sachsmedia.com>
2015-05-01 16:45:30 -0400

    tidyed scripts and tracking

--- wp-content/themes/parapxl-framework/header.php
+++ wp-content/themes/parapxl-framework/header.php
@@ -19,26 +19,11 @@
 
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 		<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/resources/images/icons/favicon.ico" />
-		<?php wp_head();?>
+
 		<!--[if lt IE 9]>
 			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
-		<!-- unfortunatly necessary due to inline scripts that rely on jQuery... -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-		<script type="text/javascript"> var _ss = _ss || []; _ss.push(['_setAccount', 'KOI-BGI58S']); _ss.push(['_trackPageView']); (function() { var ss = document.createElement('script'); ss.type = 'text/javascript'; ss.async = true; ss.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'koi-4UXZO.sharpspring.com/client/ss.js?ver=1.1.1'; var scr = document.getElementsByTagName('script')[0]; scr.parentNode.insertBefore(ss, scr); })(); </script>
-		<script>(function(){
-				window._fbds = window._fbds || {};
-				_fbds.pixelId = 1392381684358712;
-				var fbds = document.createElement('script');
-				fbds.async = true;
-				fbds.src = '//connect.facebook.net/en_US/fbds.js';
-				var s = document.getElementsByTagName('script')[0];
-				s.parentNode.insertBefore(fbds, s);
-			})();
-			window._fbq = window._fbq || [];
-			window._fbq.push(["track", "PixelInitialized", {}]);
-		</script>
-		<noscript><img height="1" width="1" border="0" alt="" style="display:none" src="https://www.facebook.com/tr?id=1392381684358712&amp;ev=NoScript" /></noscript>
 	</head>
 
 	<body <?php body_class(); ?>>
--- wp-content/themes/ronsachs/templates/default/footer.php
+++ wp-content/themes/ronsachs/templates/default/footer.php
@@ -33,13 +33,9 @@
 
 <?php if (is_front_page() ):?><script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/resources/js/min/owl.carousel.min.js"></script><?php endif;?>
 <script type="text/javascript" src="<?php echo home_url();?>/wp-content/themes/ronsachs/resources/js/min/site.min.js" async="async" defer="defer"></script>
-<script type="text/javascript">
-/* <![CDATA[ */
-var JQLBSettings = {"fitToScreen":"0","resizeSpeed":"400","displayDownloadLink":"0","navbarOnTop":"0","loopImages":"","resizeCenter":"","marginSize":"0","linkTarget":"_self","help":"","prevLinkTitle":"previous image","nextLinkTitle":"next image","prevLinkText":"\u00ab Previous","nextLinkText":"Next \u00bb","closeTitle":"close image gallery","image":"Image ","of":" of ","download":"Download"};
-/* ]]> */
-</script>
 
 <!-- adroll.com tracking -->
+<!--
 <script type="text/javascript">
 adroll_adv_id = "CREP3LIB2NHCDPBDCETA4K";
 adroll_pix_id = "7T2IUC42TNDEFG5IVCC6MN";
@@ -57,24 +53,39 @@ window.onload = function(){
    if(oldonload){oldonload()}};
 }());
 </script>
+-->
 <!-- /adroll.com tracking -->
+<!-- SharpSpring -->
+	<script type="text/javascript"> var _ss = _ss || []; _ss.push(['_setAccount', 'KOI-BGI58S']); _ss.push(['_trackPageView']); (function() { var ss = document.createElement('script'); ss.type = 'text/javascript'; ss.async = true; ss.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'koi-4UXZO.sharpspring.com/client/ss.js?ver=1.1.1'; var scr = document.getElementsByTagName('script')[0]; scr.parentNode.insertBefore(ss, scr); })(); </script>
+	<script>(function(){
+			window._fbds = window._fbds || {};
+			_fbds.pixelId = 1392381684358712;
+			var fbds = document.createElement('script');
+			fbds.async = true;
+			fbds.src = '//connect.facebook.net/en_US/fbds.js';
+			var s = document.getElementsByTagName('script')[0];
+			s.parentNode.insertBefore(fbds, s);
+		})();
+		window._fbq = window._fbq || [];
+		window._fbq.push(["track", "PixelInitialized", {}]);
+	</script>
+	<noscript><img height="1" width="1" border="0" alt="" style="display:none" src="https://www.facebook.com/tr?id=1392381684358712&amp;ev=NoScript" /></noscript>
+<!-- /SharpSpring -->
 
 <!-- Conversion Pixels (FB) -->
-<script type="text/javascript">
-var fb_param = {};
-fb_param.pixel_id = '6007260729204';
-fb_param.value = '0.00';
-(function(){
-  var fpw = document.createElement('script');
-  fpw.async = true;
-  fpw.src = (location.protocol=='http:'?'http':'https')+'://connect.facebook.net/en_US/fp.js';
-  var ref = document.getElementsByTagName('script')[0];
- ref.parentNode.insertBefore(fpw, ref);
-})();
-</script>
-<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/offsite_event.php?id=6007260729204&amp;value=0" /></noscript>
+	<script type="text/javascript">
+	var fb_param = {};
+	fb_param.pixel_id = '6007260729204';
+	fb_param.value = '0.00';
+	(function(){
+	  var fpw = document.createElement('script');
+	  fpw.async = true;
+	  fpw.src = (location.protocol=='http:'?'http':'https')+'://connect.facebook.net/en_US/fp.js';
+	  var ref = document.getElementsByTagName('script')[0];
+	 ref.parentNode.insertBefore(fpw, ref);
+	})();
+	</script>
+	<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/offsite_event.php?id=6007260729204&amp;value=0" /></noscript>
 <!-- /Conversion Pixels (FB) -->
-
-
 	</footer>
 </div>
