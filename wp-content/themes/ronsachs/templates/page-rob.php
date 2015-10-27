9013e4099e05fce542f16f6d2a530e84188b5017
brianotoole <botoole518@gmail.com>
2015-10-27 16:32:21 -0400

    added Rob's badges & bio

--- wp-content/themes/ronsachs/templates/page-rob.php
+++ wp-content/themes/ronsachs/templates/page-rob.php
@@ -38,8 +38,9 @@
 					<!--end facebook subscribe-->
 
 					<!--about paragraph-->
-					<p></p>
+					<p>Rob Orr is a web developer who thrives on building great outcomes. Having built numerous websites over the last 10 years, he specializes in building highly functional, aethestically stunning and results-oriented web projects. Rob has a solid background working with new and emerging web and business technology, having spent several years in business development and sales prior to embarking on his current path as a web developer. Originally from Atlanta, Georgia, Rob grew up in Orlando. In 2003 he married his best friend and in 2005 he and Paige had a daughter, Peyton.</p>
 					<!--end about paragraph-->
+					<p class="lastp">As a proud graduate of Florida State University, Rob is an avid supporter of the Noles and his Saturday afternoons in the fall are devoted to college football, grilling and company with family and friends. He's also a huge Atlanta Braves fan, having grown up going to games at the old Fulton County Stadium with his dad, even getting Dale Murphy's autograph during batting practice one time.</p>
 			    
 					<!--keep up with-->
 					<!--<h3 class="team">Keep Up With <?php echo get_the_title(); ?></h3>-->
@@ -60,6 +61,32 @@
 			
 				<div class="span3">
 					<!--badges section-->
+					<div id="badges">
+					<h3 class="team"><?php $title = get_the_title();$title_array = explode(' ', $title);$first_word = $title_array[0];echo $first_word;?>'s Badges</h3><!--echos first name only-->
+					<p><img src="<?php bloginfo('template_directory'); ?>/badges/code.png" /></p>
+					<p class="badge-title">Drupal &amp; Wordpress</p>
+					<p>Rob's been building with Drupal since the earliest versions of Drupal 6 and with WordPress since 2.7. How did he get things done before Drush?</p>
+					<div id="bottom-line"></div>
+
+					<p><img src="<?php bloginfo('template_directory'); ?>/badges/apple2.png" /></p>
+					<p class="badge-title">Mac Guy</p>
+					<p>Rob's not part of the cult, but mildly sympathetic to their <strike>obsession</strike> passion.</p>
+					<div id="bottom-line"></div>
+
+					<p><img src="<?php bloginfo('template_directory'); ?>/badges/family-2.png" /></p>
+					<p class="badge-title">Family Guy</p>
+					<p>Rob would rather be at Disney with his wife and daughter than anywhere else.</p>
+					<div id="bottom-line"></div>
+
+					<p><img src="<?php bloginfo('template_directory'); ?>/badges/vibin.png" /></p>
+					<p class="badge-title">Groove </p>
+					<p>Rob relies on coffee and music - from hardcore to classical - to get things done.</p>
+					<div id="bottom-line"></div>
+
+					<p><img src="<?php bloginfo('template_directory'); ?>/badges/sunshine.png" /></p>
+					<p class="badge-title">Early Bird</p>
+					<p>Rob's usually in the office around 6:00 am, long before the sun rises.</p>
+					</div>	
 					<!--end badges section-->
 				</div><!--end span 3-->
 				
