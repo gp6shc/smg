6b736164bfd7d974bc2100dd3077cf4df3581859
brianotoole <botoole518@gmail.com>
2015-10-27 15:57:19 -0400

    added page-template for Anosh bio

---
+++ wp-content/themes/ronsachs/templates/page-anosh.php
@@ -0,0 +1,69 @@
+<?php /* Template Name: Anosh */ pxl::page(); while ( have_posts() ) : the_post(); ?>
+
+<div class="row lined">
+	<div id="sidebar" class="span3">
+		<ul id="side">
+			<li><a href="<?php echo home_url( '/about' ); ?>">About Us</a></li>
+			<li class="">
+				<a href="#">Our Team</a>
+				<ul>
+					<li class="group">Executive</li>
+					<?php wp_nav_menu('menu=executive'); ?>
+					<li class="group">Client/Creative</li>
+					<?php wp_nav_menu('menu=client/creative'); ?>
+					<li class="group">Administrative</li>
+					<?php wp_nav_menu('menu=administrative'); ?>
+				</ul>
+			</li>
+		</ul>
+	</div>
+	<div class="span9">
+		<!--content-->
+		<div class="content-img"><?php the_content(); ?></div>
+		<!--end content-->
+		
+			<div class="row lined">
+				<div class="span6">
+					<!--about title-->
+					<h3 class="team">About <?php echo get_the_title(); ?></h3>
+					<!--end about title-->
+
+					<!--twitter follow
+					<p class="follow"><a href="https://twitter.com/anoshgill" class="twitter-follow-button" data-show-count="false">Follow AnoshGill</a>
+					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></p>
+					<!--end twitter follow-->
+
+                    <!--facebook subscribe
+					<p class="subscribe"><iframe src="//www.facebook.com/plugins/follow.php?href=https%3A%2F%2Fwww.facebook.com%2Fbrianzotoole&amp;layout=standard&amp;show_faces=false&amp;colorscheme=light&amp;font&amp;width=270&amp;appId=286935158076596" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:270px;" allowTransparency="true"></iframe></p>
+					<!--end facebook subscribe-->
+
+					<!--about paragraph-->
+					<p>Anosh Gill, a creative director professional and teacher of his craft, in business for more than 20 years, has provided design services for Club Med, Renaissance Aruba, Our Lucaya Bahamas, Tallahassee/Leon County Tourist Development Council, the City of Carrabelle, Visit Florida and Florida's Space Coast, as well as destination hotels and resorts including Ritz Carlton Hotels and Homes, several Marriott Hotel Resorts, Diners Club, Wyndham Hotels, Algonquin Hotel NY, The Roosevelt Hotel NY, Equinox Vermont and Claremont Hotel Berkeley. He also teaches graphic design at Florida A&M University.</p>
+					<!--end about paragraph-->
+			    
+					<!--keep up with-->
+					<!--<h3 class="team">Keep Up With <?php echo get_the_title(); ?></h3>-->
+					<!--end keep up with-->
+
+					<!--blog section
+					<h2 class="blog"><?php $title = get_the_title();$title_array = explode(' ', $title);$first_word = $title_array[0];echo $first_word;?>'s Blog Articles<!--echos first name only</h2>
+
+					<p><?php echo do_shortcode("[posts show=3 loop=post-blog category_name=anosh-gill]"); ?></p>
+					<!--end blog section
+
+					<!--more articles section-->	
+					<!--<p class="more">View More Articles by: <a href="../category/anosh-gill/"><?php echo get_the_title(); ?></a>
+					</p>-->
+					<!--end more article section-->
+
+				</div><!--end span6-->
+			
+				<div class="span3">
+					<!--badges section-->
+					<!--end badges section-->
+				</div><!--end span 3-->
+				
+		</div><!--end span9 row lined-->
+	</div><!--end span9-->
+</div><!--end container row lined-->
+<?php endwhile; pxl::page(0); ?>
